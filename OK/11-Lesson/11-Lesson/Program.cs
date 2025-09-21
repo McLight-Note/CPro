d0c598275685c28b987e379bef7f2f0f6af2e417
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-09-21 13:33:17 +0900

    +

---
+++ OK/11-Lesson/11-Lesson/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lesson11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fullName = "Muhammad McLight";
+            
+            Console.WriteLine(fullName);
+            Console.WriteLine(fullName.ToUpper());
+            Console.WriteLine(fullName.ToLower());
+            
+            String phoneNumber = "123-456-7890";
+            Console.WriteLine(phoneNumber);
+            Console.WriteLine(phoneNumber.Replace("-", "/"));
+            Console.WriteLine(phoneNumber.Replace("-", ""));
+
+            String userName = fullName.Insert(0, "@");
+            Console.WriteLine(userName);
+            
+            Console.WriteLine(fullName.Length);
+            
+            String firstName = fullName.Substring(0, 8);
+            String lastName = fullName.Substring(9, 7);
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+        }
+    }
+}
+
