90632bd4c9e8bdf7ffb03bd639f76e963e8307e0
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-09-21 12:54:42 +0900

    +

---
+++ OK/4-Lesson/4-Lesson/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    public class Exercise6
+    {
+        public static void Main(string[] args)
+        {
+            int x = 123, y = 456;
+            double height = 300.5;
+            bool alive = true;
+            char symbol = '@';
+            String name = "Muhammad"; 
+            String userName = symbol + name;
+            const double pi = 3.14;
+            
+            Console.WriteLine("Your height is " + height + "cm");
+            Console.WriteLine(x + y);
+            Console.WriteLine("Are you alive? " + alive);
+            Console.WriteLine("Your symbol is " + symbol);
+            Console.WriteLine("Hello " + name + "!");
+            Console.WriteLine("Hello " + userName + "!");
+            Console.WriteLine(pi);
+            
+            Console.ReadKey();
+            Console.Beep();
+        }
+    }
+}
---
+++ OK/5-Lesson/5-Lesson/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    public class Exercise6
+    {
+        public static void Main(string[] args)
+        {
+            // typy casting
+
+            double a = 3.14;
+            int b = Convert.ToInt32(a);
+            
+            int c = 123;
+            double d = Convert.ToDouble(c);
+
+            int e = 321;
+            String f = Convert.ToString(e);
+                
+            Console.WriteLine(b);
+            Console.WriteLine(a.GetType());
+            
+            Console.WriteLine(d);
+            Console.WriteLine(d.GetType());
+
+            Console.WriteLine(f);
+            Console.WriteLine(f.GetType());
+
+            String g = "$";
+            char h = Convert.ToChar(g);
+            Console.WriteLine(h);
+            Console.WriteLine(h.GetType());
+            
+            String i = "true";
+            bool j = Convert.ToBoolean(i);
+            Console.WriteLine(j);
+            Console.WriteLine(j.GetType());
+
+            Console.ReadKey();
+            Console.Beep();
+        }
+    }
+}
