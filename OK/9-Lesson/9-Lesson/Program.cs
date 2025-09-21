19801c20ac05ff1c416b2461cbb9574443546a28
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-09-21 13:14:25 +0900

    +

--- OK/9-Lesson/9-Lesson/Program.cs
+++ OK/9-Lesson/9-Lesson/Program.cs
@@ -6,7 +6,18 @@ namespace Lesson9
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+
+            int num = random.Next(1, 7);
+            int num2 = random.Next(1, 7);
+            int num3 = random.Next(1, 7);
             
+            // double num2 = random.NextDouble();
+            
+            Console.WriteLine(num);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+
         }
     }
 }
