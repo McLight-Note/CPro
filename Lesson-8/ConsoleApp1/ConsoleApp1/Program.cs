b4ac35575442e8d72a6b0d88cbc79a43841bfcb2
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-11-21 11:01:22 +0900

    _

---
+++ Lesson-8/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        /*
+        Console.Write("Enter first number: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = int.Parse(Console.ReadLine());
+        bool result = (a == 20 || b == 20 || (a + b == 20));
+        Console.WriteLine(result);
+        */
+        
+        Console.Write("Enter a string: ");
+        string text = Console.ReadLine();
+
+        string lower = text.ToLower();
+
+        Console.WriteLine("Lowercase: " + lower);
+        
+    }
+}
