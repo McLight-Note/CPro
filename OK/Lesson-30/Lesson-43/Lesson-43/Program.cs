337c5495e66a757cfd768ceaa8cf68a5d31cca0c
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-11-28 12:07:24 +0900

    +

---
+++ OK/Lesson-30/Lesson-43/Lesson-43/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _43_Lesson
+{
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                // interface = defines a contract that all classes inheriting from should follow
+                //             An interface declares "what class should have" and "how it should do it" 
+                // Benefit   = security + multiple inheritors + plug-and-play
+                
+                Rabbit rabbit = new Rabbit();
+                rabbit.Flee();
+                
+                Hawk hawk = new Hawk();
+                hawk.Hunt();
+                
+                Fish fish = new Fish();
+                fish.Flee();
+                fish.Hunt();
+                
+            }
+        }
+
+    interface IPrey
+    {
+        void Flee();
+    }
+
+    interface IPredator
+    {
+        void Hunt();
+    }
+    
+    class Rabbit : IPrey
+    {
+        public void Flee()
+        {
+            Console.WriteLine("Rabbit runs away");
+        }
+    }
+
+    class Hawk : IPredator
+    {
+        public void Hunt()
+        {
+            Console.WriteLine("Hawk is searching for food");
+        }
+    }
+
+    class Fish : IPrey, IPredator
+    {
+        public void Flee()
+        {
+            Console.WriteLine("Fish swims away");
+        }
+
+        public void Hunt()
+        {
+            Console.WriteLine("Fish is searching for smaller fish");
+        }
+    }
+}
+
---
+++ OK/Lesson-42/Lesson-42/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace _42_Lesson
+{
+    /*
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // polymorphism - Greek word "to have many forms"
+            //                objects can be identified by many types
+            //                EX. Cat can be: Organism, Animal, Mammal
+            
+            Car car = new Car();
+            Bycicle bycicle = new Bycicle();
+            Boat boat = new Boat();
+            
+            Vehicle[] vehicles = {car,  bycicle, boat};
+
+            foreach (Vehicle vehicle in vehicles)
+            {
+                vehicle.Go();
+            }
+        }
+    }
+
+    class Vehicle
+    {
+        public virtual void Go()
+        {
+            
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bycicle : Vehicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("Bycicle is moving");
+        }
+    }
+
+    class Boat : Vehicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("Boat is moving");
+        }
+    }
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Airplane airplane = new Airplane();
+            Train train = new Train();
+            Motorcycle motorcycle = new Motorcycle();
+            
+            Vehicle[] vehicles = {airplane, train, motorcycle};
+
+            foreach (Vehicle vehicle in vehicles)
+            {
+                vehicle.Go();
+            }
+            StartEngine(new Airplane());
+            StartEngine(new Train());
+            StartEngine(new Motorcycle());
+            foreach (Vehicle vehicle in vehicles)
+            {
+                vehicle.Stop();
+            }
+            StopEngine(new Airplane());
+            StopEngine(new Train());
+            StopEngine(new Motorcycle());
+        }
+        static void StartEngine(Vehicle vehicle)
+        {
+            vehicle.Go();
+        }
+        static void StopEngine(Vehicle vehicle)
+        {
+            vehicle.Stop();
+        }
+    }
+
+    class Vehicle
+    {
+        public virtual void Go(){}
+        public virtual void Stop(){}
+    }
+
+    class Airplane : Vehicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("Airplane is moving");
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine("Airplane is landing");
+        }
+    }
+
+    class Train : Vehicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("Train is moving");
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine("Train is stopping at the station");
+        }
+    }
+
+    class Motorcycle : Vehicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("Motorcycle is moving");
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine("Motorcycle is barking");
+        }
+    }
+}
