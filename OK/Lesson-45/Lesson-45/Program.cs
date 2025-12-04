89a0192da3afa6631286a79506bac440217ca943
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-12-05 08:53:24 +0900

    +

---
+++ OK/Lesson-45/Lesson-45/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _45_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Player> players = new List<Player>();
+            
+            players.Add(new Player("Chad"));
+            players.Add(new Player("John"));
+            players.Add(new Player("Brian"));
+
+            foreach (Player player in players)
+            {
+                Console.WriteLine(player);
+            }
+        }
+    }
+
+    class Player
+    {
+        public String username;
+
+        public Player(String username)
+        {
+            this.username = username;
+        }
+
+        public override string ToString()
+        {
+            return username;
+        }
+    }
+}
---
+++ OK/Lesson-46/Lesson-46/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _45_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // getters and setters = add security to fields by encapsulation
+            // properties = combine aspects of both fields
+            // get accessor = used to return property value
+            // set accessor = used to assign new value
+            // value keyword = defines the value being assigned be the set (parameter)
+            Car car = new Car(400);
+            car.Speed =11213123;
+            
+            Console.WriteLine(car.Speed);
+            
+
+        }
+    }
+
+    class Car
+    {
+        private int speed;
+
+        public Car(int speed)
+        {
+            Speed = speed;
+        }
+
+        public int Speed
+        {
+            get { return speed; } //read
+            set                   // write
+            {
+                if (value > 500)
+                {
+                    value = 500;
+                }
+                else
+                {
+                    speed = value;
+                }
+            }
+        }
+    }
+}
